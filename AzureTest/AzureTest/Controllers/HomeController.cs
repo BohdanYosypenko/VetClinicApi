8aa0306886f2e72d54880405eb8f338e9b3d2994
BohdanYosypenko <idybp28@gmail.com>
2021-02-15 18:31:34 +0200

    space

--- AzureTest/AzureTest/Controllers/HomeController.cs
+++ AzureTest/AzureTest/Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace AzureTest.Controllers
         public IActionResult Privacy()
         {
             return View();
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
