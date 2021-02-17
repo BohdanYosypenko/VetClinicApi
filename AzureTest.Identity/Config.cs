75f96a75039315c2a832bead9d4e3da9d7ff276b
BohdanYosypenko <idybp28@gmail.com>
2021-02-17 14:16:46 +0200

    sdfasdf

---
+++ AzureTest.Identity/Config.cs
@@ -0,0 +1,106 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServerRoles
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                // expanded version if more control is needed
+                new ApiResource
+                {
+                    Name = "VetClinicApi",
+
+                    // secret for using introspection endpoint
+                    ApiSecrets =
+                    {
+                        new Secret("angular_secret".Sha256())
+                    },
+                    
+                    // include the following using claims in access token (in addition to subject id)
+                    UserClaims = { JwtClaimTypes.Name, JwtClaimTypes.Email },
+
+                    // defines scopes
+                    Scopes =
+                    {
+                       "ApiOne",
+
+                    },
+                }
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("scope1"),
+                new ApiScope("scope2"),
+                new ApiScope("ApiOne"),
+                new ApiScope("offline_access"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+
+            new Client[]
+            {
+                // m2m client credentials flow client
+                new Client()
+                {
+                    ClientId = "client_id",
+                    ClientSecrets = {new Secret("client_secret".ToSha256()) },
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    RedirectUris = { "https://oauth.pstmn.io/v1/browser-callback",
+                                    "https://localhost:44346/"},
+
+                    AllowedScopes = {"ApiOne"},
+                    RequireConsent = false,
+                },
+
+                new Client()
+                {
+                    ClientId = "angular_client",
+                    RequirePkce = true,
+                    ClientSecrets = {new Secret("angular_secret".ToSha256()) },
+                    RequireClientSecret = false,
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://oauth.pstmn.io/v1/browser-callback",
+                                       "https://localhost:5001/signin-oidc",
+                    "http://localhost:4200", "http://localhost:4200/silent-renew.html"},
+
+                    PostLogoutRedirectUris = { "https://localhost:5001/signout-callback-oidc",
+                    "http://localhost:4200"},
+                    AllowedCorsOrigins = { "http://localhost:4200" },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "ApiOne",
+                        "offline_access",
+                    },
+
+                    RequireConsent = false,
+
+                    AllowOfflineAccess = true,
+
+                    RefreshTokenUsage =  TokenUsage.OneTimeOnly,
+
+                    AbsoluteRefreshTokenLifetime = 3600 * 72,
+                    AccessTokenLifetime = 3600,
+
+                    AccessTokenType = AccessTokenType.Reference,
+                    
+                },
+            };
+    }
+}
--- AzureTest.Identity/Startup.cs
+++ AzureTest.Identity/Startup.cs
@@ -1,56 +1,81 @@
+using IdentityServer4.Services;
+using IdentityServerRoles;
+using IdentityServerRoles.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using VetClinic.DAL;
+using VetClinic.DAL.Entities;
 
 namespace AzureTest.Identity
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
         {
+            Environment = environment;
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>()
+                .AddDefaultTokenProviders();
+
+            var builder = services
+                .AddIdentityServer(options =>
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+
+                    options.EmitStaticAudienceClaim = true;
+                })
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiResources(Config.ApiResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients);                
+
+            services.AddAuthentication(IdentityConstants.ApplicationScheme);
+
+            services.AddScoped<IProfileService, ProfileService>();
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (Environment.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();                
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseIdentityServer();
             app.UseAuthorization();
+
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
