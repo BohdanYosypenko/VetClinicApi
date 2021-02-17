0e48c91e78aed89065fb6e464d07ba696e13768f
BohdanYosypenko <idybp28@gmail.com>
2021-02-17 11:28:54 +0200

    213

---
+++ AzureTest.Identity/Quickstart/Account/AccountOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public static class AccountOptions
+    {
+        public static bool AllowLocalLogin = true;
+        public static bool AllowRememberLogin = true;
+        public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
+
+        public static bool ShowLogoutPrompt = true;
+        public static bool AutomaticRedirectAfterSignOut = false;
+
+        public static string InvalidCredentialsErrorMessage = "Invalid username or password";
+        public static string DeletedUserErrorMessage = "This user is deleted";
+    }
+}
